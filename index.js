dc5e15305919b4f32c6a2ff5a97d0013299c04db
Adil Batuhan Şahin <adilsahin3897@gmail.com>
2024-09-05 19:03:29 +0300

    npm

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 var http = require("http");
 var fs = require("fs");
+
 var server = http.createServer((req, res) => {
-    if (req.url == "") {
+    if (req.url == "/") {
         fs.readFile("index.html", (err, html) => {
             res.write(html);
             res.end();
         });
         
     }
-    else if (req.url == "/urunler") {
+    else if (req.url == "/products") {
 
         fs.readFile("urunler.html", (err, html) => {
             res.write(html);
