5217cc28971f4b55bb437b94d56988e6af45e4d2
Adil Batuhan Şahin <adilsahin3897@gmail.com>
2024-09-06 18:53:27 +0300

    nodejs routing

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
 
+//routes
+
+app.use("/products/:id/", function (req, res) {
+    res.send("products details" + req.params.id);
+});
+
+app.use("/products", function (req, res) {
+    res.send("urunler");
+});
+
+app.use("/", function (req, res) {
+    res.send("anasayfa");
+});
+
 app.listen(3000, () => {
     console.log("listening on port 3000");
 });
