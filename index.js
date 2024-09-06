8f96713d916f2ff1c580770e09634b3d725e84b7
Adil Batuhan Şahin <adilsahin3897@gmail.com>
2024-09-06 18:31:42 +0300

    express server

--- index.js
+++ index.js
@@ -1,29 +1,6 @@
-var http = require("http");
-var fs = require("fs");
+const express = require("express");
+const app = express();
 
-var server = http.createServer((req, res) => {
-    if (req.url == "/") {
-        fs.readFile("index.html", (err, html) => {
-            res.write(html);
-            res.end();
-        });
-        
-    }
-    else if (req.url == "/products") {
-
-        fs.readFile("urunler.html", (err, html) => {
-            res.write(html);
-            res.end();
-        });
-    }
-    else {
-        fs.readFile("404.html", (err, html) => {
-            res.write(html);
-            res.end();
-        });
-    }
-});
-
-server.listen(3000, () => {
-    console.log("node.js server at port 3000");
+app.listen(3000, () => {
+    console.log("listening on port 3000");
 });
