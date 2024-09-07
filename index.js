798a474398d12caa3162a2d8da704876ffd0320c
Adil Batuhan Şahin <adilsahin3897@gmail.com>
2024-09-07 21:55:25 +0300

    dynamic data

--- index.js
+++ index.js
@@ -3,14 +3,22 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+const data = [
+    { id: 1, name: "bmw", price: 10000 },
+    { id: 2, name: "mercedes", price: 20000 },
+    { id: 3, name: "audi", price: 30000 }
+]
+
 //routes
 
 app.use("/products/:id/", function (req, res) {
-    res.render("urun-details");
+    res.render("product-details");
 });
 
 app.use("/products", function (req, res) {
-    res.render("urunler");
+    res.render("products", {
+        urunler: data
+    });
 });
 
 app.use("/", function (req, res) {
