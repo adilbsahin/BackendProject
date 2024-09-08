e5f6655963aee1ab5c088f382a11ff2b3c486fd3
Adil Batuhan Şahin <adilsahin3897@gmail.com>
2024-09-08 21:14:41 +0300

    condition with ejs

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express();
 app.set("view engine", "ejs");
 
 const data = [
-    { id: 1, name: "bmw", price: 10000 },
-    { id: 2, name: "mercedes", price: 20000 },
-    { id: 3, name: "audi", price: 30000 }
+    { id: 1, name: "bmw", price: 10000, ishere: true},
+    { id: 2, name: "mercedes", price: 20000, ishere: false},
+    { id: 3, name: "audi", price: 30000, ishere: true}
 ]
 
 //routes
