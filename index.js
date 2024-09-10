9e43c5a1539fd9c55ac691220e0e95b6dbbf936d
Adil Batuhan Şahin <adilsahin3897@gmail.com>
2024-09-10 20:11:19 +0300

    express.js static files

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(express.static('public'));
 
 const data = [
-    { id: 1, name: "bmw", price: 10000, ishere: true},
-    { id: 2, name: "mercedes", price: 20000, ishere: true},
-    { id: 3, name: "audi", price: 30000, ishere: true}
+    { id: 1, name: "bmw", price: 10000, ishere: true, imageUrl: "3.jpeg" },
+    { id: 2, name: "mercedes", price: 20000, ishere: true, imageUrl: "1.jpeg" },
+    { id: 3, name: "audi", price: 30000, ishere: true, imageUrl: "2.jpeg" }
 ]
 
 //routes
